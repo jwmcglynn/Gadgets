ef58de75752859fd46c092a9c2af0beba96ab679
Jeff McGlynn <jeff@jeffmcglynn.com>
2009-01-27 05:40:39 -0800

    - Removed unneeded code in Lens Refraction. - Fixed Cairo building.

--- Lens Refraction/backend/lens_backend.cpp
+++ Lens Refraction/backend/lens_backend.cpp
@@ -34,46 +34,6 @@ bool circle::ray_intersect(vector2dd start, vector2dd dir, vector2dd& intersecti
 	if (ray_intersect_validate(start, dir, t_2, intersection, normal)) return true;
 	
 	return false;
-	
-	/*
-	f64 d = (start.x * (start.y + dir.y)) - (start.y * (start.x + dir.x));
-	f64 discriminant = radius * radius - d * d;
-	if (discriminant <= 0.0) return false;
-	
-	
-	f64 sqrt_disc = sqrt(discriminant);
-	
-	// The value that is either subtracted or added to the first half of the quadratic formula.
-	vector2dd operand(
-		sign_(dir.y) * dir.x * sqrt_disc,
-		abs_(dir.y) * sqrt_disc
-	);
-	
-	vector2dd intersection_pt(d * dir.y, -d * dir.x);
-	vector2dd other_int = intersection_pt;
-	
-	intersection_pt -= operand;
-	other_int += operand;
-	
-	f64 angle_1 = atan2(intersection_pt.y, intersection_pt.x);
-	f64 angle_2 = atan2(other_int.y, other_int.x);
-	
-	bool angle_1_good = on_ray(start, dir, intersection_pt) && angle_in_range(angle_1);
-	bool angle_2_good = on_ray(start, dir, intersection_pt) && angle_in_range(angle_2);
-	
-	// Pick which intersection is good.
-	if (!angle_1_good) {
-		if (!angle_2_good) return false;
-		intersection_pt = other_int;
-	}
-	
-	// Determine the angle on the circle that the intersection occurs at.
-	f64 angle = atan2(intersection_pt.y, intersection_pt.x);
-	
-	// There is an intersection, set the normal.
-	intersection.set(intersection_pt + center);
-	normal.set(cos(angle), sin(angle));
-	return true;*/
 }
 
 /**
